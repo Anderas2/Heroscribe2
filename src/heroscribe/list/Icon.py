07ba118d66c09373b5b953ba9b980cc315415a3f
Anderas2 <wagenerandi@gmail.com>
2019-03-20 19:49:32 +0100

    First version of the Java Icon class

---
+++ src/heroscribe/list/Icon.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+""""
+  HeroScribe2
+  Copyright (C) 2019 Shane Adams and Andreas Wagener
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published
+  by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+"""
+
+
+
+from pathlib import Path
+
+class Icon():
+    def __init__(self, filepath,
+                 xoffset,
+                 yoffset,
+                 original,
+                 image):
+        self.path = Path(filepath)
+        self.xoffset = xoffset
+        self.yoffset = yoffset
+        self.original = original
+        self.image = image
