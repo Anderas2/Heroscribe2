d4ba1a739d70a592182959519b2bd6add27a4da0
Anderas2 <wagenerandi@gmail.com>
2019-03-23 07:07:35 +0100

    First try of the Constants.py. If we want to change the corridor color of the EU version, it is here!

---
+++ src/heroscribe/Constants.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+""""
+  HeroScribe2
+  Copyright (C) 2019 Andreas Wagener and Shane Adams
+  Heroscribe 1 was by Flavio Chierichetti and Valerio Chierichetti
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published
+  by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+"""
+class Constants():
+    def __init__(self):
+        self.applicationName = "HeroScribe"
+        self.version = "1.0"
+        self.applicationVersionSuffix = "pre1"
+
+        self.europeCorridorColor = (246, 246, 246, 255)
+        self.usaCorridorColor = (246, 246, 246, 255)
+
+        self.europeDarkColor = (204, 204, 204, 255)
+        self.usaDarkColor = (178, 178, 178, 255)
+
+        self.europeTrapColor = (0, 0, 0, 0)
+        self.usaTrapColor = (250, 125, 51, 255)
+
+        self.preferencesFile = "Preferences.xml"
+
+
