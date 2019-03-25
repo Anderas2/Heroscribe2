b8e17a75076543fe9e4cac02ba2a66c76a8b511d
StratosVX <stratosvx@gmail.com>
2019-03-25 12:37:05 -0600

    Addded more buttons. Changed button output to display in status bar.

--- src/heroscribe/gui/test_gui.py
+++ src/heroscribe/gui/test_gui.py
@@ -14,43 +14,84 @@ class hs2_window(QMainWindow):
         self.height = 768
         self.initUI()
 
+	# Initialize the GUI to the main window
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left,self.top, self.width, self.height)
         self.statusBar().showMessage('Map Editor')
 
+		#--------------------Buttons--------------------#
+		# NOTE: I'm thinking these might be better at the top of the screen, then
+		# we will have the section specific buttons/boxes on the left side
         # Add Objects button
-        add_button = QPushButton('Add object', self)
-        add_button.setToolTip('This button adds elements to the map')
+        add_button = QPushButton('Add Objects', self)
+        add_button.setToolTip('Add elements to the map')
         add_button.move(20, 20)
         add_button.clicked.connect(self.add_click)
 
-        # Select/Remove Object button
-        select_button = QPushButton('Select', self)
-        select_button.setToolTip('This button allows objects to be selcted or removed from the map')
-        select_button.move(20, 50)
-        select_button.clicked.connect(self.select_click)
+        # Edit Object button
+        edit_button = QPushButton('Edit Objects', self)
+        edit_button.setToolTip('Edit placed objects')
+        edit_button.move(20, 50)
+        edit_button.clicked.connect(self.edit_click)
 
-        # Darken button
-        dark_button = QPushButton('Darken', self)
-        dark_button.setToolTip('This button darkens squares on the map')
+        # Darken/Color button
+        dark_button = QPushButton('Darken/Color', self)
+        dark_button.setToolTip('Darken/color squares on the map')
         dark_button.move(20, 80)
         dark_button.clicked.connect(self.dark_click)
+        
+        # Import button
+        import_button = QPushButton('Import', self)
+        import_button.setToolTip('Import icons, maps or quests')
+        import_button.move(20, 110)
+        import_button.clicked.connect(self.import_click)
+        
+        # Export button
+        export_button = QPushButton('Export', self)
+        export_button.setToolTip('Export icons, maps, or quests')
+        export_button.move(20, 140)
+        export_button.clicked.connect(self.export_click)
+        
+        # Questimator button
+        quest_button = QPushButton('Questimator', self)
+        quest_button.setToolTip('Questimator functionality')
+        quest_button.move(20, 170)
+        quest_button.clicked.connect(self.quest_click)
 
         # Display the window
         self.show()
 
+	#--------------------Button Event Handlers--------------------#
+	# Add Object button
     @pyqtSlot()
     def add_click(self):
-        print('Add button pressed')
-
+        self.statusBar().showMessage('Add button pressed')
+	
+	# Edit Object button
     @pyqtSlot()
-    def select_click(self):
-        print('Select/Remove button pressed')
+    def edit_click(self):
+        self.statusBar().showMessage('Edit button pressed')
 
+	# Darken/Color button
     @pyqtSlot()
     def dark_click(self):
-        print('Darken button pressed')
+        self.statusBar().showMessage('Darken button pressed')
+        
+    # Import button
+    @pyqtSlot()
+    def import_click(self):
+    	self.statusBar().showMessage('Import functionality hopefully coming soon!')
+    	
+    # Export button
+    @pyqtSlot()
+    def export_click(self):
+    	self.statusBar().showMessage('Export functionality hopefully coming soon!')
+    	
+    # Questimator button
+    @pyqtSlot()
+    def quest_click(self):
+    	self.statusBar().showMessage('Questimator functionality hopefully coming soon!')
 
 
 if __name__ == '__main__':
