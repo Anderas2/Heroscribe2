8d99990d9a78bc661944b922b31028fe969beb18
Anderas2 <wagenerandi@gmail.com>
2019-03-30 10:26:44 +0100

    commented the import of the quest module for the moment

--- src/heroscribe/quest/QBoard.py
+++ src/heroscribe/quest/QBoard.py
@@ -18,7 +18,7 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 """
 
-from src.heroscribe.quest.Quest import Quest
+#from src.heroscribe.quest.Quest import Quest
 from src.heroscribe.quest.QObject import QObject
 
 
