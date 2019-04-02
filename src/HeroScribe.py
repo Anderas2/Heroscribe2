89a7d04700b2698b6dbc7c8b87e5225a8122c22f
Anderas2 <wagenerandi@gmail.com>
2019-04-02 05:49:54 +0200

    started the main routine to test if the objects are loaded as they should be

--- src/HeroScribe.py
+++ src/HeroScribe.py
@@ -1,10 +1,22 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Mar 31 07:46:18 2019
+""""
+  HeroScribe2
+  Copyright (C) 2019 Andreas Wagener and Shane Adams
+  Heroscribe 1 was by Flavio Chierichetti and Valerio Chierichetti
 
-@author: Andreas
-"""
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published
+  by the Free Software Foundation.
 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+"""
 #import src.heroscribe.
 from src.heroscribe.helper.OS_Helper import OS_Helper
 
@@ -22,17 +34,19 @@ class HeroScribe():
         self.osh = OS_Helper()
         if self.osh.isMac :
             # set mac specific things here
-
             pass
+
         self.osh.errorprint("starting up.")
         self.preferences = Preferences()
+        self.osh.errorprint("reading objects...")
+
         self.objects = listread("Objects.xml").object_list
-        self.osh.errorprint("objects read")
+        self.osh.errorprint("objects read.")
         self.quest = Quest(width = 1,
                              height = 1,
                              board = self.objects.get_board())
         self.osh.errorprint("Quest loaded. Starting GUI.")
-        #self.gui = gui()
+        self.gui = gui()
         #self.osh.errorprint("Gui done.")
 
 
